306095f72713e224384e25dfab0a31381f92f81c
sfratini <sebastian.fratini@gmail.com>
2017-08-14 17:00:30 -0300

    Revert "test"
    This reverts commit 38a87dcd27767cff66f33dbba9aa95881d09402c.

--- 911-MotoLocator-Integration/target/generated-sources/xjc/mls-client/ar/com/telefonica/autogen/ws/mls/client/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.11 
-// Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2017.08.14 a las 03:41:12 PM ART 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.motorola.com/mls", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package ar.com.telefonica.autogen.ws.mls.client;
